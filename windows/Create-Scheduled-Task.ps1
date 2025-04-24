25db712e153174eeb7c8c00d3a59d2f73bcd4e6c
jegron <julien.egron@phenisys.com>
2025-04-24 15:43:32 +0200

    Add Create-Scheduled-Task.ps1 example

---
+++ windows/Create-Scheduled-Task.ps1
@@ -0,0 +1,19 @@
+﻿
+$User = "YOURDOMAIN\youruser"
+$Credential = Get-Credential -UserName $User -Message "Enter your password for $User"
+$PlainPassword = $Credential.GetNetworkCredential().Password
+
+$Action = New-ScheduledTaskAction -Execute "YourPath\Phenisys\Agent_MSTeams_windows_x64.exe" -Argument '--config "YourPath\Phenisys\config.yaml"'
+
+$Trigger = New-ScheduledTaskTrigger -AtStartup
+
+$Settings = New-ScheduledTaskSettingsSet `
+            -ExecutionTimeLimit (New-TimeSpan -Days 3) `
+            -StartWhenAvailable:$true `
+            -MultipleInstances "IgnoreNew" -RunOnlyIfIdle:$false
+
+$Principal = New-ScheduledTaskPrincipal -UserId $User -LogonType Password -RunLevel Highest
+
+$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings -Principal $Principal
+
+Register-ScheduledTask -TaskName "MSTeamsObservabilityAgent" -InputObject $Task -User $Credential.UserName -Password $PlainPassword -Force
